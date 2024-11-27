795e78c5fc48741fa1e886d125be4a2de47010bc
Joffrey.2018 <blanquet.joffrey2018@gmail.com>
2024-11-27 14:47:09 +0100

    Description des changements

---
+++ jour15/asset/js/style1.js
@@ -0,0 +1,45 @@
+// Exercice 1
+ 
+
+// Vérification de l'égalité :
+
+// Utilisez console.log() pour vérifier l'égalité entre 1 et "1".
+// Utilisez console.log() pour vérifier l'égalité STRICTE entre 1 et "1".
+ 
+
+// Vérification de la longueur d'un prénom :
+
+// Créez un programme qui demande le prénom d'une personne.
+// Si la longueur du prénom est inférieure à 5 caractères, affichez une alerte avec le message "Ton nom est trop court".
+ 
+
+// Addition et Vérification de la réponse :
+
+// Affichez une addition à l'utilisateur.
+// Demandez-lui de résoudre l'addition.
+// Si la réponse est correcte, félicitez-le. Sinon, dites-lui que la réponse est incorrecte.
+ 
+
+// Multiplication et Vérification de la réponse :
+
+// Affichez une multiplication à l'utilisateur.
+// Demandez-lui de résoudre la multiplication.
+// Si la réponse est correcte, félicitez-le en indiquant la bonne réponse. Sinon, dites-lui à combien d'unités il était de la bonne réponse.
+ 
+
+// Ajout d'éléments dans un tableau :
+
+// Créez un programme qui permet d'ajouter des éléments dans un tableau via des prompts.
+// Quand le tableau contient 3 éléments, renvoyez une alerte avec le contenu du tableau.
+ 
+
+// Classification des chiffres dans des bacs :
+
+// Créez un programme qui permet à l'utilisateur de mettre des chiffres dans des bacs.
+// Si le chiffre est plus grand ou égal à 12, il va dans le bac "grandNombres". Sinon, il va dans le bac "petitNombres".
+// Affichez ensuite une seule alerte avec le contenu des deux bacs.
+ 
+
+// Exercice Inventé :
+
+// Inventez un exercice en utilisant les concepts appris en JavaScript jusqu'à présent, en incluant des conditions
---
+++ jour15/asset/js/style3.js
@@ -0,0 +1,29 @@
+// Exercice 1
+ 
+
+// Stockez dans une variable, via un prompt, un jour de la semaine
+// . À l'aide d'un switch, affichez ce que vous avez planifié de faire ce jour-ci.
+// La console devra afficher : “On est lundi. Tu as cours à MolenGeek et tu as du sport en soirée.”
+ 
+
+// Exercice 2
+ 
+
+// Déclarer une variable qui prendra comme valeur un type de météo
+// À l'aide d'un switch, affichez une réaction appropriée au temps
+// Ex: Il pleut. Je prend un parapluie
+ 
+
+// Exercice 3
+ 
+
+// Déclarez une variable dont la valeur sera donnée par l'utilisateur.
+// Faites un switch, si la variable donnée est divisible par 2, faite une alert qui renvoit "Le nombre est un multiple de 2" sinon "Le nombre n'est pas un multiple de 2" ou encore si l'utilisateur n'a pas rentré un nombre "Ceci n'est pas un nombre"
+ 
+
+// Exercice 4
+ 
+
+// Déclarez trois variables que l'utilisateur remplira : nbr1 / operateur(+-/*) / nbr2;
+// Faite switch qui en fonction de l'opérateur donné, effectuera le calcul avec cet opérateur et afficher le résultat.
+// Ex:nbr1 = 1 | operateur = "+" | nbr2 = 3  => affichera 4
