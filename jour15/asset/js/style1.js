e701adb33b6d5db7ddfb6560e85cebdf599e9d32
Joffrey.2018 <blanquet.joffrey2018@gmail.com>
2024-11-28 11:45:11 +0100

    Ton message de commit ici

--- jour15/asset/js/style1.js
+++ jour15/asset/js/style1.js
@@ -4,19 +4,57 @@
 // Vérification de l'égalité :
 
 // Utilisez console.log() pour vérifier l'égalité entre 1 et "1".
+// let nbr="1"
+
+// if (nbr==1) { 
+//     console.log(nbr "est vrai"); 
+// } else { 
+//     console.log("votre nombre n'est égale à 1");
+// }
+
+
 // Utilisez console.log() pour vérifier l'égalité STRICTE entre 1 et "1".
- 
+// if (1 === "1") { 
+//     console.log( nbr +"est vrai");
+// } else {
+//     console.log( nbr+ "est faux");
+// }   
+// }
+
 
 // Vérification de la longueur d'un prénom :
 
 // Créez un programme qui demande le prénom d'une personne.
-// Si la longueur du prénom est inférieure à 5 caractères, affichez une alerte avec le message "Ton nom est trop court".
+// let prenom=prompt("quel est votre prenom")
+// // Si la longueur du prénom est inférieure à 5 caractères, affichez une alerte avec le message "Ton nom est trop court".
  
+// if (prenom.length<5) {
+//     alert("Ton nom est trop court")
+// } else {
+//     alert("il a une longueur raisonnable")
+// }
+
 
 // Addition et Vérification de la réponse :
 
 // Affichez une addition à l'utilisateur.
 // Demandez-lui de résoudre l'addition.
+// let nombre1 = Math.floor(Math.random() * 100);
+// let nombre2 = Math.floor(Math.random() * 100);
+
+// // Calculer le résultat de l'addition
+// let resultat = nombre1 + nombre2;
+
+// // Afficher l'addition et demander la réponse à l'utilisateur
+// let reponse = parseInt(prompt(`Quelle est la somme de ${nombre1} + ${nombre2} ?`));
+
+// // Vérifier si la réponse est correcte
+// if (reponse === resultat) {
+//     alert("Félicitations ! Votre réponse est correcte.");
+// } else {
+//     alert(`Incorrect. La bonne réponse était ${resultat}.`);
+// }
+
 // Si la réponse est correcte, félicitez-le. Sinon, dites-lui que la réponse est incorrecte.
  
 
@@ -26,19 +64,65 @@
 // Demandez-lui de résoudre la multiplication.
 // Si la réponse est correcte, félicitez-le en indiquant la bonne réponse. Sinon, dites-lui à combien d'unités il était de la bonne réponse.
  
+// let nombre1 = Math.floor(Math.random() * 11);
+// let nombre2 = Math.floor(Math.random() * 11);
+
+// // Calculer le résultat de l'addition
+// let resultat = nombre1 * nombre2;
+
+// // Afficher l'addition et demander la réponse à l'utilisateur
+// let reponse = parseInt(prompt(`Quelle est la multiplication de ${nombre1} * ${nombre2} ?`));
+
+// // Vérifier si la réponse est correcte
+// if (reponse === resultat) {
+//     alert("Félicitations ! Votre réponse est correcte.");
+// } else {
+//     alert(`Incorrect. La bonne réponse était ${resultat}.`);
+// }
+
 
 // Ajout d'éléments dans un tableau :
-
 // Créez un programme qui permet d'ajouter des éléments dans un tableau via des prompts.
 // Quand le tableau contient 3 éléments, renvoyez une alerte avec le contenu du tableau.
  
+// let tab=["banane", "voiture","poire","danser"]
+// prompt("veillez ajouter 3 élément suplémentaire.")
+// if (tab=3*i) {
+//     alert("élément ajouter sont"+ i)
+// } else {
+    
+// }
+// let tab = ["banane", "voiture", "poire", "danser"];
+// let elementsAjoutes = 0;
+
+// function ajouterElement(message) {
+//     let nouvelElement = prompt(message);
+//     if (nouvelElement) {
+//         tab.push(nouvelElement);
+//         elementsAjoutes++;
+//     }
+// }
+
+// ajouterElement("Veuillez ajouter le premier élément supplémentaire :");
+// ajouterElement("Veuillez ajouter le deuxième élément supplémentaire :");
+// ajouterElement("Veuillez ajouter le troisième élément supplémentaire :");
+
+// if (elementsAjoutes === 3) {
+//     alert(`Les éléments du tableau sont : ${tab.join(", ")}`);
+// } else {
+//     alert("Une erreur est survenue. Veuillez réessayer.");
+// }
 
 // Classification des chiffres dans des bacs :
-
 // Créez un programme qui permet à l'utilisateur de mettre des chiffres dans des bacs.
 // Si le chiffre est plus grand ou égal à 12, il va dans le bac "grandNombres". Sinon, il va dans le bac "petitNombres".
 // Affichez ensuite une seule alerte avec le contenu des deux bacs.
- 
+let chiffre=+prompt("Choisissez un nombre entre 0 et 100")
+if (chiffre<=12) {
+    alert("Votre chiffre est dans le bac des petitNombres")
+} else {
+    alert("Votre chiffre est dans le bac des grandNombres")
+}
 
 // Exercice Inventé :
 
--- jour15/asset/js/style3.js
+++ jour15/asset/js/style3.js
@@ -4,7 +4,36 @@
 // Stockez dans une variable, via un prompt, un jour de la semaine
 // . À l'aide d'un switch, affichez ce que vous avez planifié de faire ce jour-ci.
 // La console devra afficher : “On est lundi. Tu as cours à MolenGeek et tu as du sport en soirée.”
- 
+
+// Stocker dans une variable, via un prompt, un jour de la semaine
+let jour = prompt("Veuillez entrer un jour de la semaine :").toLowerCase();
+
+// À l'aide d'un switch, afficher ce que vous avez planifié de faire ce jour-ci
+switch (jour) {
+    case "lundi":
+        console.log("On est lundi. Tu as cours à MolenGeek et tu as du sport en soirée.");
+        break;
+    case "mardi":
+        console.log("On est mardi. Tu as une réunion avec l'équipe de projet.");
+        break;
+    case "mercredi":
+        console.log("On est mercredi. Tu dois travailler sur ton projet personnel.");
+        break;
+    case "jeudi":
+        console.log("On est jeudi. C'est le jour de la présentation de ton travail.");
+        break;
+    case "vendredi":
+        console.log("On est vendredi. Tu as un rendez-vous médical l'après-midi.");
+        break;
+    case "samedi":
+        console.log("On est samedi. C'est le week-end, relaxe-toi et profite du temps libre.");
+        break;
+    case "dimanche":
+        console.log("On est dimanche. C'est le jour du repos et des activités en famille.");
+        break;
+    default:
+        console.log("Jour invalide. Veuillez entrer un jour de la semaine valide.");
+}
 
 // Exercice 2
  
@@ -13,13 +42,53 @@
 // À l'aide d'un switch, affichez une réaction appropriée au temps
 // Ex: Il pleut. Je prend un parapluie
  
+// Déclarer une variable pour le type de météo
+let meteo = prompt("Quel temps fait-il aujourd'hui ? (soleil, pluie, neige, nuages, vent)");
+
+// Utiliser un switch pour afficher une réaction appropriée au type de météo
+switch (meteo.toLowerCase()) {
+    case "soleil":
+        console.log("Il fait beau aujourd'hui. Profite du soleil !");
+        break;
+    case "pluie":
+        console.log("Il pleut. Je prends un parapluie.");
+        break;
+    case "neige":
+        console.log("Il neige. Je mets mon manteau et mes gants.");
+        break;
+    case "nuages":
+        console.log("Le ciel est nuageux. Je prends peut-être une petite veste.");
+        break;
+    case "vent":
+        console.log("Il y a du vent. Je porte une écharpe.");
+        break;
+    default:
+        console.log("Je ne sais pas quel temps il fait. Soyez préparé à tout !");
+}
 
 // Exercice 3
  
 
 // Déclarez une variable dont la valeur sera donnée par l'utilisateur.
 // Faites un switch, si la variable donnée est divisible par 2, faite une alert qui renvoit "Le nombre est un multiple de 2" sinon "Le nombre n'est pas un multiple de 2" ou encore si l'utilisateur n'a pas rentré un nombre "Ceci n'est pas un nombre"
- 
+ // Déclarez une variable dont la valeur sera donnée par l'utilisateur
+// let valeur = prompt("Veuillez entrer une valeur :");
+
+// // Convertir la valeur en nombre entier
+// let nbr = parseInt(valeur);
+
+// switch (true) {
+//     case isNaN(nbr):
+//         alert("Ceci n'est pas un nombre");
+//         break;
+//     case (nbr % 2 === 0):
+//         alert("Le nombre est un multiple de 2");
+//         break;
+//     default:
+//         alert("Le nombre n'est pas un multiple de 2");
+//         break;
+// }
+
 
 // Exercice 4
  
@@ -27,3 +96,35 @@
 // Déclarez trois variables que l'utilisateur remplira : nbr1 / operateur(+-/*) / nbr2;
 // Faite switch qui en fonction de l'opérateur donné, effectuera le calcul avec cet opérateur et afficher le résultat.
 // Ex:nbr1 = 1 | operateur = "+" | nbr2 = 3  => affichera 4
+
+// Déclarez trois variables que l'utilisateur remplira
+// let nbr1 = parseFloat(prompt("Veuillez entrer le premier nombre :"));
+// let operateur = prompt("Veuillez entrer un opérateur (+, -, *, /) :");
+// let nbr2 = parseFloat(prompt("Veuillez entrer le deuxième nombre :"));
+
+// let resultat;
+
+// // Utiliser un switch pour effectuer le calcul en fonction de l'opérateur donné
+// switch (operateur) {
+//     case "+":
+//         resultat = nbr1 + nbr2;
+//         break;
+//     case "-":
+//         resultat = nbr1 - nbr2;
+//         break;
+//     case "*":
+//         resultat = nbr1 * nbr2;
+//         break;
+//     case "/":
+//         if (nbr2 !== 0) {
+//             resultat = nbr1 / nbr2;
+//         } else {
+//             resultat = "Erreur : Division par zéro";
+//         }
+//         break;
+//     default:
+//         resultat = "Opérateur invalide";
+// }
+
+// // Afficher le résultat
+// alert(`Le résultat de ${nbr1} ${operateur} ${nbr2} est : ${resultat}`);
